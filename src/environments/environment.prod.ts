ebe9adf07dd58e2d03681423ac577a533e93028f
alicarlo <alicarlo.montijop@gmail.com>
2026-03-17 10:43:52 -0700

    Remove environment files from repo

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  // apiUrl: 'https://machucas-api.naatteam.com'// 'https://automotriz-api.naatteam.com'
-  apiUrl: 'https://automotriz-api.naatteam.com'
-};
